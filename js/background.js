8321e34c6a6b93e919ab989ae6102a4db6a8b625
Kenan Sulayman <kenan@sly.mn>
2015-09-28 01:02:51 +0200

    General cleanup

--- js/background.js
+++ js/background.js
@@ -1,20 +1,29 @@
 var movieList = [];
+
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.cmd == 'addMovie') {
-      var isNew = true;
-      for (var i = 0; i < movieList.length; i++) {
-        if (movieList[i].title == request.title) {
-          isNew = false;
-        }
-      }
-      if (isNew) {
-        movieList.push({title: request.movieName, url: request.movieURL});
-        sendResponse({msg: request.cmd});
-      } else {
-        sendResponse({msg: request.cmd + ' movie exists'});
-      }
-      chrome.storage.local.set({'MovieStorage': movieList});
-    }
-  }
+	function(request, sender, sendResponse) {
+		if (request.cmd === 'addMovie') {
+			var knownMovie = movieList.some(function (movie) {
+				return movie.title === request.title;
+			});
+
+			if (knownMovie) {
+				sendResponse({
+					msg: request.cmd + ' movie exists'
+				});
+			} else {
+				movieList.push({
+					title: request.movieName,
+					url: request.movieURL
+				});
+				sendResponse({
+					msg: request.cmd
+				});
+			}
+
+			chrome.storage.local.set({
+				'MovieStorage': movieList
+			});
+		}
+	}
 );
--- js/menu.js
+++ js/menu.js
@@ -1,65 +1,102 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.body.style.margin = '0px';
-  chrome.storage.local.get('MovieStorage', function (result) {
-    var movieList = result.MovieStorage;
-    var content = '<li><div>Movies:</div></li>';
-    for (var i = 0; i < movieList.length; i++) {
-      content = content +
-      '<li>' +
-        '<div class="movieItem">' +
-          '<p id="movieTitle">' + movieList[i].title + '</p>' +
-          '<ul id="shortcutMenu">' +
-            '<li class="shortcut fa fa-download" x-action="download" x-title="' + movieList[i].title + '" x-url="' + movieList[i].url + '">' +
-            '<li class="shortcut fa fa-external-link" x-action="copyLink" x-title="' + movieList[i].title + '" x-url="' + movieList[i].url + '">' +
-            '<li class="shortcut fa fa-ban" x-action="remove" x-title="' + movieList[i].title + '" x-url="' + movieList[i].url + '">' +
-          '</ul>' +
-      '</li>';
-    }
-    document.getElementById('movieList').innerHTML = content;
-    var listElements = document.getElementsByClassName('shortcut');
-    for (var j = 0; j < listElements.length; j++) {
-      switch(listElements[j].getAttribute('x-action')) {
-      case 'download':
-        listElements[j].addEventListener('click', function() {
-          var element = document.createElement('a');
-          element.setAttribute('href', this.getAttribute('x-url'));
-          element.setAttribute('download', this.getAttribute('x-title') + '.mkv');
-          element.style.display = 'none';
-          document.body.appendChild(element);
-          element.click();
-          document.body.removeChild(element);
-        });
-        break;
-      case 'copyLink':
-        listElements[j].addEventListener('click', function() {
-          const input = document.createElement('input');
-          input.style.position = 'fixed';
-          input.style.opacity = 0;
-          input.value = this.getAttribute('x-url');
-          document.body.appendChild(input);
-          input.select();
-          document.execCommand('Copy');
-          document.body.removeChild(input);
-        });
-        break;
-      case 'remove':
-        listElements[j].addEventListener('click', function() {
-          chrome.storage.local.get('MovieStorage', function (result) {
-            var toBeSliced = 0;
-            var movieList = result.MovieStorage;
-            for (var i = 0; i < movieList.length; i++) {
-              if (movieList[i].title == this.getAttribute('x-title')) {
-                toBeSliced = i + 1;
-                var parent = this.parentNode.parentNode.parentNode.parentNode;
-                parent.removeChild(this.parentNode.parentNode.parentNode);
-              }
-            }
-            var newList = movieList.slice(toBeSliced);
-            chrome.storage.local.set({'MovieStorage': newList});
-          }.bind(this));
-        });
-        break;
-      }
-    }
-  });
+	document.body.style.margin = '0px';
+
+	chrome.storage.local.get('MovieStorage', function(result) {
+		var movieList = result.MovieStorage;
+		
+		var content = '<li><div>Movies:</div></li>';
+
+		var movieItem = function (movie) {
+			return '<li>' +
+					'<div class="movieItem">' +
+						'<p id="movieTitle">' + movie.title + '</p>' +
+						'<ul id="shortcutMenu">' +
+							'<li class="shortcut fa fa-download" x-action="download" x-title="' + movie.title + '" x-url="' + movie.url + '">' +
+							'<li class="shortcut fa fa-external-link" x-action="copyLink" x-title="' + movie.title + '" x-url="' + movie.url + '">' +
+							'<li class="shortcut fa fa-ban" x-action="remove" x-title="' + movie.title + '" x-url="' + movie.url + '">' +
+						'</ul>' +
+					'</div>' +
+				'</li>';
+		}
+
+		movieList.forEach(function (movie) {
+			content += movieItem(movie);
+		})
+
+		document.getElementById('movieList').innerHTML = content;
+
+		var listElements = document.getElementsByClassName('shortcut');
+
+		var downloadClick = function() {
+			var element = document.createElement('a');
+			    element.setAttribute('href', this.getAttribute('x-url'));
+			    element.setAttribute('download', this.getAttribute('x-title') + '.mkv');
+			    element.style.display = 'none';
+			
+			document.body.appendChild(element);
+			
+			element.click();
+			
+			document.body.removeChild(element);
+		};
+
+		var copyLinkClick = function() {
+			var input = document.createElement('input');
+			    input.style.position = 'fixed';
+			    input.style.opacity = 0;
+			    input.value = this.getAttribute('x-url');
+			
+			document.body.appendChild(input);
+			
+			input.select();
+			
+			document.execCommand('Copy');
+			document.body.removeChild(input);
+		};
+
+		var removeClick = function() {
+			var self = this;
+
+			chrome.storage.local.get('MovieStorage', function(result) {
+				var toBeSliced = 0;
+				var movieList = result.MovieStorage;
+
+				movieList.forEach(function (movie) {
+					if (movie.title == self.getAttribute('x-title')) {
+						toBeSliced = i + 1;
+						var enclosure = self.parentNode.parentNode.parentNode,
+						    enclosureParent = enclosure.parentNode;
+						
+						enclosureParent.removeChild(enclosure);
+					}
+				})
+
+				var newList = movieList.slice(toBeSliced);
+
+				chrome.storage.local.set({
+					'MovieStorage': newList
+				});
+			});
+		};
+
+		var listElementHandler = function (action) {
+			if (action === 'download')
+				return downloadClick;
+
+			if (action === 'copyLink')
+				return copyLinkClick;
+
+			if (action === 'remove')
+				return removeClick;
+		}
+
+		listElements.forEach(function (listElement) {
+			var action = listElement.getAttribute('x-action');
+			    actionHandler = listElementHandler(action);
+
+			if (actionHandler) {
+				return listElement.addEventListener('click', actionHandler);
+			}
+		})
+	});
 });
